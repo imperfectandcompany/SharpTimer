d77f07612a5c7db65f38d61223ff0394c1acb74a
hazmat321 <132187457+hazmat321@users.noreply.github.com>
2024-05-14 21:43:10 -0700

    Update Utils.cs
    Point reduction

--- src/Plugin/Utils.cs
+++ src/Plugin/Utils.cs
@@ -218,7 +218,7 @@ namespace SharpTimer
 
         public double CalculatePoints(int timerTicks)
         {
-            double basePoints = 10000.0;
+            double basePoints = 1000.0;
             double timeFactor = 0.0001;
             double tierMult = 0.1;
 
@@ -233,7 +233,7 @@ namespace SharpTimer
 
         public double CalculatePBPoints(int timerTicks)
         {
-            double basePoints = 10000.0;
+            double basePoints = 1000.0;
             double timeFactor = 0.01;
             double tierMult = 0.1;
 
